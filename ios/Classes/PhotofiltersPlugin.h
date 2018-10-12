bdcae1d367a1b38fe23b90c2b3d6d3983177757f
Sharafudheen <skkallayath@gmail.com>
2018-10-12 22:11:02 +0530

    removed old library

--- ios/Classes/PhotofiltersPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface PhotofiltersPlugin : NSObject<FlutterPlugin>
-@end
--- lib/geometry.dart
+++
@@ -1,9 +0,0 @@
-class Point {
-  double x;
-  double y;
-  Point(this.x, this.y);
-
-  List<double> toList() {
-    return [x, y];
-  }
-}
