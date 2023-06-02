d92c88ec1b396eac71cf8b066afde4bbe76d6f71
jinosh05 <jinoshprabhuraj5@gmail.com>
2023-06-02 08:54:37 +0530

    flutter_lints rules followed

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -7,12 +7,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../lib/main.dart';
+import 'package:photofilters_example/main.dart';
 
 void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that platform version is retrieved.
     expect(
